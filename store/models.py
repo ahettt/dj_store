5b3913376dae81c1ddd65189fde67dd113ca3bb7
ahettt <bondara254@gmail.com>
2026-02-08 12:39:46 +0200

    Add store app and Product/Category models

---
+++ store/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, verbose_name="Название категории")
+    slug = models.SlugField(max_length=200, unique=True, verbose_name="URL (слаг)")
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE, verbose_name="Категория")
+    name = models.CharField(max_length=200, verbose_name="Наименование")
+    slug = models.SlugField(max_length=200, unique=True, verbose_name="URL (слаг)")
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True, verbose_name="Изображение")
+    description = models.TextField(blank=True, verbose_name="Описание")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
+    available = models.BooleanField(default=True, verbose_name="В наличии")
+    created = models.DateTimeField(auto_auto_now_add=True, verbose_name="Дата создания")
+    updated = models.DateTimeField(auto_now=True, verbose_name="Дата обновления")
+
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
+        ordering = ('-created',)
+
+    def __str__(self):
+        return self.name
