32ecdb3c08c26a2aa649b2991e89a06d2364c103
ahettt <bondara254@gmail.com>
2026-02-23 12:14:39 +0200

    Add pagination to product list

--- store/views.py
+++ store/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 from cart.forms import CartAddProductForm
 from django.db.models import Q
+from django.core.paginator import Paginator
+
 
 def product_list(request, category_slug=None):
     category = None
@@ -18,10 +20,16 @@ def product_list(request, category_slug=None):
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
 
+    paginator = Paginator(products, 9)
+
+    page_number = request.GET.get('page')
+
+    page_obj = paginator.get_page(page_number)
+
     return render(request, 'store/product_list.html', {
         'category': category,
         'categories': categories,
-        'products': products,
+        'page_obj': page_obj,
         'query': query
     })
 
