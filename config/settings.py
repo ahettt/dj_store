8521fcfcc929f904f34c6a2595bfb7e52ad3a3a5
ahettt <bondara254@gmail.com>
2026-03-31 11:14:44 +0300

    fix: add Vercel domain to CORS allowed origins

--- config/settings.py
+++ config/settings.py
@@ -164,6 +164,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://dj-store-frontend.vercel.app",
+    "https://overpro.fit",
 ]
 CORS_ALLOW_CREDENTIALS = True
 
