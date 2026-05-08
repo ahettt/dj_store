5d3beecee2a440954336d39b27750f6762685adc
ahettt <bondara254@gmail.com>
2026-05-08 11:03:06 +0300

    feat: add Cart and CartItem models

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from store.models import Product
 
-# Create your models here.
+class Cart(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='cart')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Корзина пользователя {self.user.username}"
+
+class CartItem(models.Model):
+    # Товары привязаны к конкретной корзине
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} шт. - {self.product.name}"
