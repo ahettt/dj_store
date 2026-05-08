8343de9b1998ac7df6dddcd0f32c65341a6ed96c
ahettt <bondara254@gmail.com>
2026-05-08 10:21:15 +0300

    fix: remove old HTML register route to fix 500 error

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,5 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('register/', views.register, name='register'),
     path('api/register/', views.RegisterAPIView.as_view(), name='api_register'),
 ]
