f59f69fefd192c4217d333cf4a97d9e21c5f384f
ahettt <bondara254@gmail.com>
2026-05-08 10:15:44 +0300

    feat: implement seamless AuthModal and AuthProvider for JWT authentication flow

--- config/settings.py
+++ config/settings.py
@@ -177,5 +177,8 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
 }
