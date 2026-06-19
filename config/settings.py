ff794c09f3dc6da01916d3e99da2e6a0dd130804
ahettt <bondara254@gmail.com>
2026-06-19 14:37:27 +0300

    Fix: A domain has been added to the ALLOWED_HOSTS list

--- config/settings.py
+++ config/settings.py
@@ -27,6 +27,7 @@ SECRET_KEY = 'django-insecure-#&+j3a5-mk48^0k%l0eqwfaf-+7jan6#7=x3i7q!&13l$_0hr^
 DEBUG = True
 
 ALLOWED_HOSTS = ['api.overpro.fit', '157.180.28.141', 'localhost', '127.0.0.1', '157.180.28.141']
+CSRF_TRUSTED_ORIGINS = ['https://django-ecommerce.overpro.fit']
 
 
 # Application definition
