db62b6160cb88caad1c5f398fcba4b4027393d64
ahettt <bondara254@gmail.com>
2026-06-20 16:48:49 +0300

    Fix: добавил защиту от огромных чисел и текста в корзине

--- cart/views.py
+++ cart/views.py
@@ -13,7 +13,14 @@ def cart_add(request, product_id):
     cart = Cart(request)
     product = get_object_or_404(Product, id=product_id)
 
-    quantity = int(request.POST.get('quantity', 1))
+    try:
+        quantity = int(request.POST.get('quantity', 1))
+        if quantity < 1:
+            quantity = 1
+        elif quantity > 99:
+            quantity = 99
+    except ValueError:
+        quantity = 1
 
     override = request.POST.get('override', False)
     if override in ['True', 'true', '1', 'on']:
